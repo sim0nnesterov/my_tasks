ec45f710b2b08de2936f09b01688c67ec25e5e4b
sim0nnesterov <sem0nnesterov@gmail.com>
2024-12-17 20:17:57 +0300

    nt

---
+++ tool/srt.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define fr first
+#define sc second
+
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using dbl = double;
+using pii = pair<int, int>;
+
+const ll INF = 1e18;
+const ld RAM = 1e-9;
+const int MOD = 1e9 + 7;
+const ld EPS = 1e-18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr); 
+#ifdef ARCH 
+    freopen("input.txt", "r", stdin);
+    //? freopen("output.txt", "w", stdout);
+#endif
+    
+    return 0;
+}           
+//! OS: ArchLinux ~ notebook  
